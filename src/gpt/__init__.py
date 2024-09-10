2602d8c6d5f1b6ee338e2220d7902325f4ae3be4
avivilloz <avivilloz@gmail.com>
2024-09-10 16:46:40 +0300

    ur

--- src/gpt/__init__.py
+++ src/gpt/__init__.py
@@ -1,2 +1,1 @@
-from .setup import setup_logging
 from .gpt import prompt_gpt
--- src/gpt/utils.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def printnl(text: str):
-    print(f"{text}\n")
-
-
-def create_dir(dir_path):
-    os.makedirs(dir_path, exist_ok=True)
